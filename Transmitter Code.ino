f7d823228ea45e771ab695d6a29f5c1c208bf5a8
Tushar Banswal <47272349+TusharBanswal@users.noreply.github.com>
2019-04-28 23:35:46 +0530

    Update Transmitter Code.ino
    433Mhz link Wireless health Monitoring system..
    Working of this project is quite easy but a little calculation for calculating heart rate is required. There are several methods for calculating heart rate, but here we have read only five pulses. Then we have calculated total heart beat in a minute by applying the below formula:
    
         Five_pusle_time=time2-time1;
    
          Single_pulse_time= Five_pusle_time /5;
    
          rate=60000/ Single_pulse_time;
    
    where time1 is first pulse counter value
    
    time2 is list pulse counter value
    
    rate is final heart rate.
    
    ##Now come to connection part :
    Using arduino=
      make sure Transmitter data pin is connected to pin 11 of arduino uno.
        Lcd control pins are connected to arduino as Rs=8 ,E= 6,D4= 5,D5=4 ,D6=3  ,D7=2. And ground RW pin of LCD.
        Output Of heart Beat Sensor is connected to Pin no. 9 of arduino uno.
        Output Of LM35 temperature sensor is connected to Analog input '2' pin of arduino.
      
    Using atmega328p controller=
       Note that PINOUT of controller is different from arduino pins . Follow datasheet or pinout to differentiate digital and PWM pins .

--- Transmitter Code.ino
+++ Transmitter Code.ino
@@ -1,11 +1,11 @@
-#include <RH_ASK.h>
-#include<LiquidCrystal.h>
-// sets the interfacing pins // Creates an LC object. Parameters: (rs, enable, d4, d5, d6, d7) //
+#include <RH_ASK.h>  //include radiohead library
+#include<LiquidCrystal.h> //include liquid crystal library
+
 LiquidCrystal lcd(8, 6, 5, 4, 3, 2);  // sets the interfacing pins // Creates an LC object. Parameters: (rs, enable, d4, d5, d6, d7) //
 
-RH_ASK driver;
-char buffer[24];
-int temp;
+RH_ASK driver;   //creating object driver of class
+char buffer[24];  // setting buffer size length
+int temp; 
 int in = 9;
 int i=0,k=0,rate=0;
 
@@ -14,13 +14,13 @@ unsigned long time;
 
 
 void setup()
-{ pinMode(in, INPUT);
+{ pinMode(in, INPUT);  //make pin 9 as input pin
  
- lcd.begin(16,2);
+ lcd.begin(16,2);  //initialize 16*2 lcd display
  
   delay(500);
-  Serial.begin(9600);   // Debugging only
-  if (!driver.init())
+  Serial.begin(9600);   //set up serial monitor
+  if (!driver.init()) // Debugging only
     Serial.println("init failed");
 }
 
@@ -28,7 +28,7 @@ void loop()
 {
 
     k=0;
-    while(k<7)
+    while(k<7)  // description given above.
     {
      if(digitalRead(in))
      {
@@ -41,39 +41,28 @@ void loop()
       time2=millis();
       rate=time2-time1;
       rate=rate/5;
-      rate=60000/rate;  
+      rate=60000/rate;  // here we are getting heart rate 
 
 
      temp = analogRead(A2);
     int mv=(temp/1024.0)*5000;
-    int cel=mv/10;
+    int cel=mv/10;   // here we are getting temperature in celsius
 
       
 
       lcd.setCursor(0,0);
       lcd.print("HeartRate=");
       
-      lcd.print(rate);
+      lcd.print(rate);  //printing heart rate to the lcd display
         lcd.print("_BPM");
       lcd.setCursor(0,1);
       lcd.print("Temp=");
-      lcd.print(cel);
+      lcd.print(cel);   //printing temperature to lcd display
       lcd.print(" *C");
       delay(1000);
    
-  sprintf(buffer, "%d,%d", rate,cel);
+  sprintf(buffer, "%d,%d", rate,cel);  // converting integer to string; as we cannot transmit integer directly through network 
   
-    lcd.clear();
-      lcd.setCursor(0,0);
-      lcd.print("HeartRate=");
-      
-      lcd.print(rate);
-        lcd.print("_BPM");
-      lcd.setCursor(0,1);
-      lcd.print("Temp=");
-      lcd.print(cel);
-      lcd.print(" *C");
-      delay(1000);
   driver.send((uint8_t *)buffer, strlen(buffer)); 
   driver.waitPacketSent();
   delay(500);
