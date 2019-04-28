c444ac1ee282a821cd0c14fcf7d213eaf68ea639
Tushar Banswal <47272349+TusharBanswal@users.noreply.github.com>
2019-04-28 22:50:04 +0530

    Update Receiver Code.ino

--- Receiver Code.ino
+++ Receiver Code.ino
@@ -13,56 +13,51 @@
 LiquidCrystal lcd(8, 6 , 5, 4, 3, 2);  // sets the interfacing pins // Creates an LC object. Parameters: (rs, enable, d4, d5, d6, d7) //
 RH_ASK rf_driver;
  
-char StringReceived[22];
+char StringReceived[22];   //setting maximum string length
 int a,b;
 void setup()
 {
    
-    lcd.setCursor(0,0);
-
-    
+    lcd.setCursor(0,0);    
     delay(100);
 
-    lcd.begin(16,2);
-    rf_driver.init();
-    // Setup Serial Monitor
-    Serial.begin(9600);
+    lcd.begin(16,2);    //initializing LCD 16*2 display
+    rf_driver.init();      //calling class driver initialization function
+    
+    Serial.begin(9600);    // Setup Serial Monitor
 }
  
 void loop()
 {   
-    // Set buffer to size of expected message
+    
    
-    uint8_t buf[24];
+    uint8_t buf[24];    // Set buffer to size of expected message
     uint8_t buflen = sizeof(buf);
-    // Check if received packet is correct size
-    if (rf_driver.recv(buf, &buflen))
+   
+    if (rf_driver.recv(buf, &buflen))    // Check if received packet is correct size
     {
        
  int i;
         // Message with a good checksum received, dump it. 
         for (i = 0; i < buflen; i++)
  {            
-          // Fill Sensor1CharMsg Char array with corresponding 
-        
-                   
-          // chars from buffer.   
-          StringReceived[i] = char(buf[i]);
+              
+          StringReceived[i] = char(buf[i]);  // chars from buffer. 
  }
  
       sscanf(StringReceived, "%d,%d",&a,&b); // Converts a string to an array
          
         // Turn off light to and await next message 
-      Serial.println(StringReceived);
+      Serial.println(StringReceived);  //printing received string to serial monitor
       lcd.clear();
       lcd.setCursor(0,0);
       lcd.print("HeartRate=");
       
-      lcd.print(a);
+      lcd.print(a);          //printing the value of heart beat to LCD
         lcd.print("_BPM");
       lcd.setCursor(0,1);
       lcd.print("Temp=");
-      lcd.print(b);
+      lcd.print(b);           //printing the value temperature to the lcd
       lcd.print(" *C");
       
  
